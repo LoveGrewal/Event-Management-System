6ed1a8e8fe82c7d995fedbf1cfcaa417a314725c
Loveshant Grewal <loveshantgrewal1102@gmail.com>
2019-06-04 00:08:49 -0400

    UDP Servers with multithreading

---
+++ src/concordia/dems/servers/TorontoUDPServer.java
@@ -0,0 +1,63 @@
+package concordia.dems.servers;
+
+        import java.io.IOException;
+        import java.net.DatagramPacket;
+        import java.net.DatagramSocket;
+        import java.net.InetAddress;
+        import java.net.SocketException;
+
+public class TorontoUDPServer implements Runnable {
+    private static DatagramSocket aSocket;
+    public void run() {
+        try{
+            aSocket = new DatagramSocket(8889);
+            byte[] buffer;
+            DatagramPacket request;
+            while (true) {
+                buffer = new byte[1000];
+                System.out.println("Server waiting for request at 8888 ............");
+                request = new DatagramPacket(buffer, buffer.length);
+                aSocket.receive(request);
+                //byte[] b = new String(request.getData()).getBytes();
+                //new Thread(new Responder(b, request.getLength(), request.getAddress(), request.getPort(), aSocket)).start();
+                new Thread(new TorontoResponder(request.getData(), request.getLength(), request.getAddress(), request.getPort(), aSocket)).start();
+            }
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO: " + e.getMessage());
+        } finally {
+            if (aSocket != null)
+                aSocket.close();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new TorontoUDPServer().run();
+    }
+}
+
+class TorontoResponder implements Runnable {
+
+    DatagramSocket aSocket;
+    DatagramPacket reply;
+
+    public TorontoResponder(byte[] data, int length, InetAddress add, int port, DatagramSocket aSocket) {
+        //this.request = request;
+        this.aSocket = aSocket;
+        reply = new DatagramPacket(data, length, add, port);
+        System.out.println("Data :"+ new String(reply.getData()));
+    }
+
+    public void  run() {
+        try{
+            aSocket.send(reply);
+        }catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO: " + e.getMessage());
+        }
+    }
+}
+
